204172046751dd6e1376e27649aa4ac7a6f6f975
Elizabeth Lee <65786964+elzlee@users.noreply.github.com>
2020-12-03 18:56:42 -0500

    incorrect BFS implementation

--- easy_level.py
+++ easy_level.py
@@ -1,12 +1,18 @@
 ##########################################
 # Easy maze level
-# maze generation: binary tree 
+# Perfect maze generation: binary tree
+# Maze Searching: Breadth First Search 
+# Pathfinding: made it up? not sure if it's a thing
 ##########################################
+# Citations referenced in this file #
+    # hackerearth: 'Pseudocode' section, 'Visualizer' section
+    # https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/tutorial/
+
+    # geeksforgeeks: 
+    # https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/?ref=leftbar-rightbar
 '''
-BUG: penulimate mint square never erases
-BUG: "solved!" text doesn't show even when mode.userSolvedMaze = True.
+QUESTION: popping things for set
 BUG: going back to menu saves progress
-BUG: the squares on maze look very uneven? Am I blind
 
 - create maze without canvas, stored as list/tuple
 - choose start, end points
@@ -63,9 +69,23 @@ class EasyMode(Mode):
         # user, path
         mode.userPosition = (mode.mazeRows-1, mode.mazeCols-1) #update with row, drow, etc.
         mode.userPath = set() # to be filled...
+        mode.userPath.add((mode.mazeRows-1, mode.mazeCols-1)) # add first position
         mode.userStartedMaze = False
         mode.userSolvedMaze = False
+        mode.startingCell = (mode.mazeRows-1, mode.mazeCols-1) # mint) change for diff maze gen algos
+
+        # pathfinding: solution, hint
+        mode.showSolution = False
+        mode.showHint = False
+        mode.currNode = (mode.mazeRows-1, mode.mazeCols-1) # init
+        mode.visited = [[False] * mode.mazeCols for i in range(mode.mazeRows)] # 2d list of maze cells, True=visited
+
+        # mode.solution = [[False] * mode.mazeCols for i in range(mode.mazeRows)] # 2d list of maze cells
+        mode.nodeSearchDirections = [NORTH, EAST, SOUTH, WEST] 
+        mode.solution  = []
+        mode.createSolution()
         
+
     def getCellBounds(mode, row, col): 
             # returns (x0, y0, x1, y1) corners/bounding box of given cell in grid
             x0 = mode.mazeTopLeftCornerX +  col * mode.cellSize
@@ -76,13 +96,28 @@ class EasyMode(Mode):
 
     ####################### user interaction #######################
     def mousePressed(mode, event):
+        # click on cell
+        
+        # BUTTON: 'hint'
+        b1cx = (mode.mazeCX - (mode.mazeCols * mode.cellSize) / 2) / 2
+        b1cy = mode.mazeCY
+        if (((b1cx - mode.buttonW/2) <= event.x <= (b1cx + mode.buttonW/2)) and
+            ((b1cy - mode.buttonH/2) <= event.y <= (b1cy + mode.buttonH/2))):
+            mode.showHint = True
+
+        # BUTTON: 'solution'
+        b2cx = mode.width - b1cx
+        b2cy = mode.mazeCY
+        if (((b2cx - mode.buttonW/2) <= event.x <= (b2cx + mode.buttonW/2)) and
+            ((b2cy - mode.buttonH/2) <= event.y <= (b2cy + mode.buttonH/2))):
+            mode.showSolution = True
+
         # BUTTON: 'menu'
         b3cx = mode.width/2
         b3cy = mode.height - 50 
         if (((b3cx - mode.buttonW/2) <= event.x <= (b3cx + mode.buttonW/2)) and 
             ((b3cy - mode.buttonH/2) <= event.y <= (b3cy + mode.buttonH/2))):
             mode.app.setActiveMode(mode.app.splashScreenMode)
-        # click on cell
 
     def keyPressed(mode, event):
         (row, col) = mode.userPosition
@@ -130,8 +165,9 @@ class EasyMode(Mode):
         else:
             mode.userPath.remove((row,col))
         if mode.userPosition == (0,0): 
-            mode.userSolvedMaze == True
-        print('userPosition=',mode.userPosition)
+            mode.userSolvedMaze = True
+        #print('userPosition=',mode.userPosition)
+        #print('userpath=', mode.userPath)
 
     #def undoMove(mode, row, col):
 
@@ -200,13 +236,13 @@ class EasyMode(Mode):
 
                 # draw red maze lines
                 if currCell.north == True:
-                    canvas.create_line(x0, y0, x1, y0, fill=mode.mazeWallColor, width=3)
+                    canvas.create_line(x0, y0, x1, y0, fill=mode.mazeWallColor, width=1)
                 if currCell.east == True:
-                    canvas.create_line(x1, y0, x1, y1, fill=mode.mazeWallColor, width=3)
+                    canvas.create_line(x1, y0, x1, y1, fill=mode.mazeWallColor, width=1)
                 if currCell.south == True:
-                    canvas.create_line(x0, y1, x1, y1, fill=mode.mazeWallColor, width=3)
+                    canvas.create_line(x0, y1, x1, y1, fill=mode.mazeWallColor, width=1)
                 if currCell.west == True:
-                    canvas.create_line(x0, y0, x0, y1, fill=mode.mazeWallColor, width=3)
+                    canvas.create_line(x0, y0, x0, y1, fill=mode.mazeWallColor, width=1)
 
                 # mint walls for start & end
                 if (row, col) == (0, 0):
@@ -214,6 +250,72 @@ class EasyMode(Mode):
                 if (row, col) == (mode.mazeRows-1, mode.mazeCols-1):
                     canvas.create_line(x0, y1, x1, y1, fill=mode.mint, width=3)
               
+    ####################### maze solving #######################
+    def drawSolution(mode, canvas):
+        
+        for (row, col) in mode.solution:
+                (x0, y0, x1, y1) = mode.getCellBounds(row, col) 
+                if mode.solution[row][col] == True:
+                    canvas.create_rectangle(x0, y0, x1, y1, fill=mode.mint, width = 0)
+
+    def createSolution(mode):
+    # Citation: hackerearth, geeksforgeeks (see Citations section at top for more details)
+        nodesList = [] # init
+        (targetRow, targetCol) = (0, 0)
+        childToParentDict = dict() # child: parent (no multiples)
+
+        def BFSearch(mode, row, col):  #row, col = starting node
+            # base cases
+            '''
+            if (row,col) in mode.visited: 
+                print('did base case1')
+                return False
+            '''
+            mode.visited.append((row,col))
+            if (row,col)==(targetRow,targetCol): 
+                print('did base case2')
+                return True
+
+            # recursive case
+            mode.visited[mode.mazeRows-1][mode.mazeCols-1] = True # source node
+            print('##############################')
+            print(f'Current cell = ({row},{col})')
+            for direction in mode.nodeSearchDirections:
+                (drow, dcol) = direction # N, E, S, W
+                (neighborRow, neighborCol) = (row+drow, col+dcol) #the neighbor node
+                
+                print('---direction=', direction, '=', (mode.validMove(row, col, direction) and not mode.visited[neighborRow][neighborCol]))
+                
+                if (mode.validMove(row, col, direction) == True and 
+                    mode.visited[neighborRow][neighborCol] == False):
+                    print('--------got to if statement!')
+                    childToParentDict[(neighborRow, neighborCol)] = (row, col) # child: parent
+                    mode.visited[neighborRow][neighborCol] = True # marked as visited, this is now (currRow, currCol)
+                    nodesList.insert(0,(neighborRow, neighborCol)) # FI of FIFO
+                    print('--------nodesList=', nodesList)
+                else:
+                    print('--------do nothing')
+            nodesList.pop(0) # FO of FIFO
+            for (nodeRow, nodeRow) in nodesList:
+                print('gonna do recursion...')
+                BFSearch(mode, nodeRow, nodeRow)
+                print('just did recursion!')
+        
+        # walking back from target (gold) --> start (mint)
+        def getSolution(mode, row, col): # (row, col) = starting cell of path
+            # base case
+            if (row, col) == (mode.mazeRows-1, mode.mazeCols-1):
+                return [(mode.mazeRows-1, mode.mazeCols-1)]
+            # recursion
+            (parentRow, parentCol) = childToParentDict[(row, col)]
+            mode.solution = [(row,col)] +  getSolution(parentRow, parentCol)
+                 
+        if BFSearch(mode, mode.mazeRows-1, mode.mazeCols-1) == True: #solution can be found from given source node
+            print('SOLUTION=', mode.solution)
+            return getSolution(mode, targetRow, targetCol)  # return (row, col) tuples of solution path
+        else: 
+            return False
+
     ####################### extra features #######################           
     def drawGameButtons(mode, canvas):
         # hint button = left
@@ -264,5 +366,7 @@ class EasyMode(Mode):
             mode.drawMintToGoldInstructions(canvas)
         if mode.userSolvedMaze == True:
             mode.drawSolvedText(canvas)
+        if mode.showSolution == True:
+            mode.drawSolution(canvas)
 
 
